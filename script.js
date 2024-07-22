495ceccd4954ca48a9b23635069f905e55fcef1a
foreverexiled <foreverexiled23@gmail.com>
2024-07-22 21:59:57 +0500

    changed to fetch instead of jq ajax

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 $(document).ready(function () {
     $('#click-me').on('click', function () {
-        $.ajax({
-            type: "get",
-            url: "http://127.0.0.1:8000/index.php",
-            data: {
-                q: '1'
-            },
-            success: function (response) {
-                let div = document.createElement('div');
-                div.innerHTML = response;
-            }
+        fetch('http://127.0.0.1:8000/index.php', {
+            method: 'get',
+            data: 'q=1'
+        }).then(function (data) { 
+            let div = document.createElement('div');
+            div.innerHTML = data;
         });
     });
 });
